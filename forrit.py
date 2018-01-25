80a0f1ef54981ff4629b7668ab4a879285204a66
Robertingi00 <robertha278@nemi.tskoli.is>
2018-01-25 10:16:20 +0000

    Update forrit.py

--- forrit.py
+++ forrit.py
@@ -51,4 +51,4 @@ def error404(error):
 @route('/static/<filename>')
 def server_static(filename):
     return static_file(filename, root='../myndir')
-run()
+run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
